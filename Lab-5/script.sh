eb63ece1ea0d6cdd7c41940a117d161b47c5a378
Girish Kailash <girishseervi891@gmail.com>
2025-03-20 10:14:16 +0530

    Add files via upload

---
+++ Lab-5/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Create a directory called "Backup" in Documents folder
+mkdir -p ~/Documents/Backup
+
+#Copy all .c files into the folder
+cp *.c ~/Documents/Backup/
+
+#'cd' into Documents folder
+cd ~/Documents
+
+#Create a tar.gz file using tar command
+tar -czf Backup.tar.gz Backup/
+
+#Delete the "Backup" folderi
+rm -rf ~/Documents/Backup
